2a7dacff3259530ecd53a35ff46ee5e6b018d42d
SARTHAK <dev.sarthak0001@gmail.com>
2025-06-23 10:48:45 +0530

    Maintaince.dart modify

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Maintenance(),
+      home: Login(),
     );
   }
 }
