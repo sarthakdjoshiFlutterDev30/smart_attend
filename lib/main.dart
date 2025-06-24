b7c780b640a1bb7431d935de1d380279fdf8a2d8
SARTHAK JOSHI <dev.sarthak0001@gmail.com>
2025-06-24 19:29:11 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Smart-Attend-Admin',
+      title: 'Smart Attendance Admin',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
